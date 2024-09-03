62f03cb21baf3b895743dee3688b85ef257de4b9
SUMMIT KAWAKAMI <summit.kawakami@getmykarma.ca>
2024-09-03 16:53:47 -0600

    improved debugging logs for prod

--- src/components/HabitsProvider/HabitsProvider.js
+++ src/components/HabitsProvider/HabitsProvider.js
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import HabitPopover from '../HabitPopover';
 import Dashboard from '../Dashboard';
 import './styles.css';
@@ -7,7 +7,7 @@ import './styles.css';
 export const HabitContext = React.createContext();
 
 function HabitsProvider({ habits, tags, user, completionRates, isAll }) {
-  const [checked, setChecked] = React.useState(() => {
+  const [checked, setChecked] = useState(() => {
     const getTodayInUserTimezone = (timezone) => {
       const date = new Date().toLocaleString('en-US', { timeZone: timezone });
       const localDate = new Date(date);
@@ -28,12 +28,41 @@ function HabitsProvider({ habits, tags, user, completionRates, isAll }) {
       initialCheckBoxes[habit.uuid] = habitLastDayLogged === today;
     });
 
+    console.log('Initializing checked state:', initialCheckBoxes);
     return initialCheckBoxes;
   });
-  console.log('checked habitprovider: ', checked);
+
+  // Logging for renders
+  useEffect(() => {
+    console.log('HabitsProvider re-rendered');
+    console.log('Current checked state:', checked);
+  });
+
+  // Optional: You might want to log when habits or user data changes
+  useEffect(() => {
+    console.log('Habits data changed:', habits);
+    console.log('User data:', user);
+  }, [habits, user]);
+
+  // Function to update the checked state and log the change
+  const updateChecked = useCallback(
+    (newState) => {
+      console.log('Updating checked state:', newState);
+      setChecked(newState);
+    },
+    [setChecked]
+  );
+
   return (
     <HabitContext.Provider
-      value={{ habits, tags, user, checked, setChecked, completionRates }}
+      value={{
+        habits,
+        tags,
+        user,
+        checked,
+        setChecked: updateChecked,
+        completionRates,
+      }}
     >
       <div className='habitsContainer'>
         <span className='add-habit'>
